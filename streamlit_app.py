2a44b72aecd8e13bc0eb3ff1b427f3cf4d7b5a17
kwagdongan <142889476+kwagdongan@users.noreply.github.com>
2026-06-13 15:39:12 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,25 @@
 import streamlit as st
 import pandas as pd
 
-import os
-# 현재 경로에 pages 폴더가 있는지 확인
-st.write(f"현재 경로: {os.getcwd()}")
-st.write(f"pages 폴더 존재 여부: {os.path.exists('pages')}")
-
 st.set_page_config(page_title="Steam 분석 앱", layout="wide")
 
 st.title("🎮 Steam 게임 태그 분석 대시보드")
 st.write("왼쪽 사이드바에서 분석 페이지를 선택하세요.")
 # 사이드바에 글자 하나만 띄워보기
 st.sidebar.title("테스트 사이드바")
+
+st.title("메인 페이지")
+
+# 버튼을 누르면 'pages/01_레드오션_분석.py'로 이동
+if st.button("레드오션 분석 페이지로 이동"):
+    st.switch_page("pages/01_레드오션_분석.py")
+
+# 버튼을 누르면 'pages/02_평점_분석.py'로 이동
+if st.button("평점 분석 페이지로 이동"):
+    st.switch_page("pages/02_평점_분석.py")
+
+
+
 @st.cache_data
 def load_data():
     return pd.read_csv("datas.csv")
